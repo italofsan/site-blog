fd324a33e94178bd12c805c8f556b8b24dd8e9b5
Italo Santos <italofsan@outlook.com>
2026-04-09 18:16:06 -0300

    Feat: Adiciona componente Active Link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Button } from '../ui/button'
+import { ActiveLink } from '../active-link'
 
 export const Header = () => {
   const router = useRouter()
@@ -15,25 +16,11 @@ export const Header = () => {
           <Link href='/'>Logo</Link>
 
           <nav className='flex items-center gap-6'>
-            <Link
-              href='/'
-              className={cn(
-                'text-sm font-medium transition-colors hover:text-blue-500',
-                isHomePage ? 'text-blue-500' : 'text-muted-foreground',
-              )}
-            >
-              Início
-            </Link>
-            <Link
-              href='/blog'
-              className={cn(
-                'text-sm font-medium transition-colors hover:text-blue-500',
-                isBlogPage ? 'text-blue-500' : 'text-muted-foreground',
-              )}
-            >
-              Blog
-            </Link>
-            <Button variant='secondary'>Começar</Button>
+            <ActiveLink href='/'>Início</ActiveLink>
+            <ActiveLink href='/blog'>Blog</ActiveLink>
+            <Button variant='secondary' asChild>
+              <Link href='/comecar'>Começar</Link>
+            </Button>
           </nav>
         </div>
       </div>
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,5 @@
+import { Header } from '@/components/header'
+
+export default function BlogPage() {
+  return <h2>Blog</h2>
+}
