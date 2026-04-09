02dfde66364a9a2980fa0869518c9a3dd64779c1
Italo Santos <italofsan@outlook.com>
2026-04-09 12:10:03 -0300

    feat: initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
+import { Geist, Geist_Mono } from 'next/font/google'
+
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+})
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+})
+
 export default function Home() {
   return (
-    <div className='min-h-screen min-w-full bg-slate-500'>
-      <h2 className='text-4xl text-green-400'>Welcome to the Blog</h2>
+    <div
+      className={`min-h-screen min-w-full bg-slate-500 ${geistSans.variable} ${geistMono.variable}`}
+    >
+      <h2 className='text-4xl text-green-400'>Hello World</h2>
     </div>
   )
 }
